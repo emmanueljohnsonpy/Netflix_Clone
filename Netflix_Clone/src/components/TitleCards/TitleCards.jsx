b74354fc160dbd580e2d5d2d68b717457949bab1
emmanueljohnsonpy <emmanueljohnsonpy@gmail.com>
2024-12-31 01:37:56 +0530

    TMDB API completed

--- Netflix_Clone/src/components/TitleCards/TitleCards.jsx
+++ Netflix_Clone/src/components/TitleCards/TitleCards.jsx
@@ -1,9 +1,18 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./TitleCards.css";
 import cards_data from "../../assets/cards/Cards_data";
 
 const TitleCards = ({ title, category }) => {
+  const [apiData, setApiData] = useState([]);
   const cardsRef = useRef();
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYzlhYzM4OWFjYTBmYmVkNjU2Y2Y2OGJhMjhjZmQwYyIsIm5iZiI6MTczNTU4MjkzMC40MzUwMDAyLCJzdWIiOiI2NzcyZTRkMjA1YzQ2YWIzZmM5MmIxMTUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.p1PqCZFVTfJM632J3NS4C4X__pAiDDYF-tPqnZxonm8",
+    },
+  };
 
   const handleWheel = (event) => {
     event.preventDefault();
@@ -11,6 +20,15 @@ const TitleCards = ({ title, category }) => {
   };
 
   useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        category ? category : "now_playing"
+      }?language=en-US&page=1`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setApiData(res.results))
+      .catch((err) => console.error(err));
     cardsRef.current.addEventListener("wheel", handleWheel);
   }, []);
 
@@ -18,11 +36,14 @@ const TitleCards = ({ title, category }) => {
     <div className="title-cards">
       <h2>{title ? title : "Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index) => {
+        {apiData.map((card, index) => {
           return (
             <div className="card" key={index}>
-              <img src={card.image} alt=""></img>
-              <p>{card.name}</p>
+              <img
+                src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path}
+                alt=""
+              ></img>
+              <p>{card.original_title}</p>
             </div>
           );
         })}
--- Netflix_Clone/src/pages/Home/Home.jsx
+++ Netflix_Clone/src/pages/Home/Home.jsx
@@ -3,8 +3,8 @@ import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar";
 import hero_banner from "../../assets/hero_banner.jpg";
 import hero_title from "../../assets/hero_title.png";
-import play_icon from "../../assets/play_icon.png"
-import info_icon from "../../assets/info_icon.png"
+import play_icon from "../../assets/play_icon.png";
+import info_icon from "../../assets/info_icon.png";
 import TitleCards from "../../components/TitleCards/TitleCards";
 import Footer from "../../components/Footer/Footer";
 
@@ -13,7 +13,7 @@ const Home = () => {
     <div className="home">
       <Navbar />
       <div className="hero">
-        <img src={hero_banner} alt="" className="banner-img" ></img>
+        <img src={hero_banner} alt="" className="banner-img"></img>
         <div className="hero-caption">
           <img src={hero_title} alt="" className="caption-img" />
           <p>
@@ -22,19 +22,23 @@ const Home = () => {
             immortal enemy.
           </p>
           <div className="hero-btns">
-            <button className="btn"><img src={play_icon} alt=""></img>Play</button>
-            <button className="btn dark-btn"><img src={info_icon} alt=""></img>More Info</button>
+            <button className="btn">
+              <img src={play_icon} alt=""></img>Play
+            </button>
+            <button className="btn dark-btn">
+              <img src={info_icon} alt=""></img>More Info
+            </button>
           </div>
-          <TitleCards/>
+          <TitleCards />
         </div>
       </div>
       <div className="more-cards">
-        <TitleCards title={"Blockbuster Movies"}/>
-        <TitleCards title={"Only on Netflix"}/>
-        <TitleCards title={"Upcoming"}/>
-        <TitleCards title={"Top Pics for You"}/>
+        <TitleCards title={"Blockbuster Movies"} category={"top_rated"} />
+        <TitleCards title={"Only on Netflix"} category={"popular"} />
+        <TitleCards title={"Upcoming"} category={"upcoming"} />
+        <TitleCards title={"Top Pics for You"} category={"now_playing"} />
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
